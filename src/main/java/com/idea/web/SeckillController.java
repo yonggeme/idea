f5b8a6f8eccccc87bddeccc53a77bbfc50f413a0
zhangyong <zhangyong@vipkid.com.cn>
2016-05-28 17:19:35 +0800

    controller

---
+++ src/main/java/com/idea/web/SeckillController.java
@@ -0,0 +1,42 @@
+package com.idea.web;
+
+import com.idea.entity.Seckill;
+import com.idea.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by zhangyong on 16/5/27.
+ */
+@Controller("/seckill")
+public class SeckillController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(name = "/list", method = RequestMethod.GET)
+    public String list(Model model){
+        List<Seckill> seckillList = seckillService.queryAll();
+        model.addAttribute("seckillList", seckillList);
+        return "list";  //WEB-INF/jsp/list.jsp
+    }
+
+    @RequestMapping(name = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String queryById(@PathVariable("seckillId") Long seckillId, Model model){
+        if (seckillId == null){
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.queryById(seckillId);
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+}
